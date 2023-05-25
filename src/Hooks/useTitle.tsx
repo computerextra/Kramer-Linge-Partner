cf4a3d5af179dddd2656d1052353c809e5b1db2a
computerextra <81916339+computerextra@users.noreply.github.com>
2023-05-25 15:26:22 +0200

    Init

---
+++ src/Hooks/useTitle.tsx
@@ -0,0 +1,10 @@
+import {useEffect} from "react";
+import {MAIN_TITLE} from "../Data/Constants";
+
+export default function useTitle(title = "") {
+  useEffect(() => {
+    if(title.length > 1) document.title = MAIN_TITLE + " - " + title;
+    else document.title = MAIN_TITLE;
+  },[title])
+  return;
+}
