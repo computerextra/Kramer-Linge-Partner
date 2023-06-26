8e8689fd3651ca5f7cdbb16a23e627070ccd6053
computerextra <81916339+computerextra@users.noreply.github.com>
2023-06-26 14:41:37 +0200

    Update Header.tsx

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -6,33 +6,23 @@ import { Urls } from "../Data/Urls";
 
 export default function Header() {
   return (
-    <>
-      <Container
-        fluid
-        className="bg-dark d-flex justify-content-center sticky-top">
-        <small className="text-white">
-          Diese Webseite dient ausschließlich Demonstrationszwecken und ist
-          nicht für den Produktiveinsatz vorgesehen.
-        </small>
-      </Container>
-      <Container className="mb-1 mt-1">
-        <NavLink to={Urls.Root}>
-          <Image
-            fluid
-            rounded
-            src="/Images/Logo_2017.webp"
-            alt="Logo"
-          />
-        </NavLink>
-        <p className="d-none d-lg-block bg-secondary float-end text-light pt-1 pb-1 ps-2 pe-2 text-center rounded">
-          <FontAwesomeIcon icon={faPhone} />{" "}
-          <a
-            href="tel:05615105760"
-            className="text-decoration-none text-light">
-            0561 / 510 576 0
-          </a>
-        </p>
-      </Container>
-    </>
+    <Container className="mb-1 mt-1">
+      <NavLink to={Urls.Root}>
+        <Image
+          fluid
+          rounded
+          src="/Images/Logo_2017.webp"
+          alt="Logo"
+        />
+      </NavLink>
+      <p className="d-none d-lg-block bg-secondary float-end text-light pt-1 pb-1 ps-2 pe-2 text-center rounded">
+        <FontAwesomeIcon icon={faPhone} />{" "}
+        <a
+          href="tel:05615105760"
+          className="text-decoration-none text-light">
+          0561 / 510 576 0
+        </a>
+      </p>
+    </Container>
   );
 }
